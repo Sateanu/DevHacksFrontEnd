f91ac1a1e2515a6da743f101abab8272fdbdef12
adrianaalexandru <adriana.alexandru8@yahoo.com>
2015-11-21 15:48:09 +0200

    finished orderdatasource

--- DevHacksApp/app/src/main/java/exception/overdose/stack/devhacksapp/database/OrderDataSource.java
+++ DevHacksApp/app/src/main/java/exception/overdose/stack/devhacksapp/database/OrderDataSource.java
@@ -3,50 +3,40 @@ package exception.overdose.stack.devhacksapp.database;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.util.Log;
-
-import net.indyvision.metronome.pojo.Connection;
-import net.indyvision.metronome.pojo.Playlist;
-import net.indyvision.metronome.pojo.Song;
 
 import java.util.ArrayList;
-
-import exception.overdose.stack.devhacksapp.models.POJO.Food;
 import exception.overdose.stack.devhacksapp.models.POJO.Orders;
 
 /**
  * Created by alexbuicescu on 17.09.2015.
  */
 public class OrderDataSource extends BaseDataSource {
-    private FoodDataSource foodDataSource;
+    private SubOrderDataSource subOrderDataSource;
 
     public OrderDataSource(Context context) {
         super(context);
         tableName = DatabaseHelper.TABLE_ORDER_NAME;
-        foodDataSource = new FoodDataSource(context);
-        foodDataSource.open();
+        subOrderDataSource = new SubOrderDataSource(context);
+        subOrderDataSource.open();
     }
 
     @Override
     public void closeHelper()
     {
         getDbHelper().close();
-        foodDataSource.closeHelper();
+        subOrderDataSource.closeHelper();
     }
 
-    public long insertSongToPlaylist(Playlist playlist, Song song, int orderInPlaylist) {
+    public long insertOrder(Orders orders) {
 
         if (!getDatabase().isOpen()) {
             open();
         }
         try {
             long rowId;
-            ContentValues contentValues = getOrderContentValues(playlist.getId(), song.getId(), orderInPlaylist);
+            ContentValues contentValues = getOrderContentValues(orders);
 
             rowId = getDatabase().insert(tableName, null, contentValues);
-
-            playlist.setSongsCounter(playlist.getSongsCounter() + 1);
-            foodDataSource.updatePlaylist(playlist);
 
             return rowId;
         } catch (Exception e) {
@@ -56,44 +46,16 @@ public class OrderDataSource extends BaseDataSource {
         return -1;
     }
 
-    public ArrayList<Connection> getAllConnections() {
-        if (!getDatabase().isOpen()) {
-            open();
-        }
-
-        ArrayList<Connection> connections = new ArrayList<>();
-        try {
-            Cursor cursor = getDatabase().query(tableName,
-                    null, null, null, null, null, null);
-
-            if (cursor.moveToFirst()) {
-                do {
-                    connections.add(cursorToConnection(cursor));
-                } while (cursor.moveToNext());
-            }
-
-            cursor.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            closeDatabase();
-        }
-
-        return connections;
-    }
-
-    public boolean updateSongInPlaylist(long playlistId, long songId, int newOrderInPlaylist) {
+    public boolean updateOrder(Orders orders) {
         if (!getDatabase().isOpen()) {
             open();
         }
         try {
-            ContentValues contentValues = getOrderContentValues(playlistId, songId, newOrderInPlaylist);
+            ContentValues contentValues = getOrderContentValues(orders);
 
             if (getDatabase().update(tableName, contentValues,
-                    DatabaseHelper.COLUMN_CONNECTIONS_PLAYLISTS_ID + " = ? and " +
-                            DatabaseHelper.COLUMN_CONNECTIONS_SONG_ID + " = ?",
-                    new String[]{String.valueOf(playlistId), String.valueOf(songId)}) <= 0) {
-                Log.e("database", songId + " not found in: " + playlistId);
+                    DatabaseHelper.COLUMN_ID + " = ? ",
+                    new String[]{String.valueOf(orders.getId())}) <= 0) {
                 return false;
             }
 
@@ -105,48 +67,15 @@ public class OrderDataSource extends BaseDataSource {
         }
         return false;
     }
-
-    private ContentValues getOrderContentValues(long playlistId, long songId, int orderInPlaylist) {
-        ContentValues contentValues = new ContentValues();
-
-        contentValues.put(DatabaseHelper.COLUMN_CONNECTIONS_PLAYLISTS_ID, playlistId);
-        contentValues.put(DatabaseHelper.COLUMN_CONNECTIONS_SONG_ID, songId);
-        contentValues.put(DatabaseHelper.COLUMN_CONNECTIONS_ORDER_IN_PLAYLIST, orderInPlaylist);
-
-        return contentValues;
-    }
-
-
-    public Integer deleteSongFromPlaylist(Playlist playlist, Song song) {
-        if (!getDatabase().isOpen()) {
-            open();
-        }
-        try {
-            int rows = getDatabase().delete(tableName,
-                    DatabaseHelper.COLUMN_CONNECTIONS_PLAYLISTS_ID + " = ? and" +
-                            DatabaseHelper.COLUMN_CONNECTIONS_SONG_ID + " = ?",
-                    new String[]{String.valueOf(playlist.getId()), String.valueOf(song.getId())});
-
-            if(rows > 0) {
-                playlist.setSongsCounter(playlist.getSongsCounter() - 1);
-                foodDataSource.updatePlaylist(playlist);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            closeDatabase();
-        }
-        return -1;
-    }
-
     public Integer deleteOrder(long orderId) {
         if (!getDatabase().isOpen()) {
             open();
         }
         try {
+            subOrderDataSource.deleteSubOrderByOrderId(orderId);
             int rows = getDatabase().delete(tableName,
-                    DatabaseHelper.COLUMN_CONNECTIONS_PLAYLISTS_ID + " = ?",
-                    new String[]{String.valueOf(playlistId)});
+                    DatabaseHelper.COLUMN_ID + " = ?",
+                    new String[]{String.valueOf(orderId)});
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -154,8 +83,6 @@ public class OrderDataSource extends BaseDataSource {
         }
         return -1;
     }
-
-
     public ArrayList<Orders> getAllOrders() {
         if (!getDatabase().isOpen()) {
             open();
